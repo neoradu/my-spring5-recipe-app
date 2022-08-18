2c773bd9aa96601c021946227457b10eed390a0d
Radu Radu <neoradu@gmail.com>
2022-08-18 14:48:11 +0300

    added command objects converters and tests
    Signed-off-by: Radu Radu <neoradu@gmail.com>

--- src/main/java/guru/springframework/services/RecipeService.java
+++ src/main/java/guru/springframework/services/RecipeService.java
@@ -1,14 +1,16 @@
 package guru.springframework.services;
 
+import guru.springframework.commands.RecipeCommand;
 import guru.springframework.domain.Recipe;
 
 import java.util.Set;
 
-/**
- * Created by jt on 6/13/17.
- */
-public interface RecipeService {
 
-    public Set<Recipe> getRecipes();
-    public Recipe findById(Long id);
+public interface RecipeService {
+    Set<Recipe> getRecipes();
+
+    Recipe findById(Long l);
+
+    RecipeCommand saveRecipeCommand(RecipeCommand command);
+
 }
